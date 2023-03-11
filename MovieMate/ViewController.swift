c0f450b93c124a442596daabaa2a748e21d07968
Juan Santiago Martín Balbás <jmbalbas@gmail.com>
2023-03-11 19:02:56 +0100

    Initial commit

---
+++ MovieMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieMate
+//
+//  Created by Juan Santiago Martín Balbás on 11/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
